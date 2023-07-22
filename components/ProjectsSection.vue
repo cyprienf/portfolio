44a803434f46628072efe91e2586c819d5013019
CyprienF <floquet.cyprien@gmail.com>
2023-07-22 15:46:49 +0200

    chore: install and configure prisma and supabase

---
+++ components/ProjectsSection.vue
@@ -0,0 +1,9 @@
+<template>
+  <div v-for="project in projects" :key="project.id">
+    {{ project.name }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const { data: projects } = await useFetch("/api/projects");
+</script>
