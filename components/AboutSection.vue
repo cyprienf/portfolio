56b5b7a2e71df941f9d6067ac440f07db0508a7f
CyprienF <floquet.cyprien@gmail.com>
2023-07-23 15:12:22 +0200

    feat: create about header section

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -1,1 +1,80 @@
-<template>OUI</template>
+<template>
+  <section class="about-section">
+    <div class="about-section__picture">
+      <img :src="picture" alt="picture" />
+    </div>
+
+    <div class="about-section__title">
+      Software engineer, front end developer and movies lover
+    </div>
+
+    <div class="about-section__bio">
+      Hi I'm Cyprien, a french frontend developer with three years of
+      experience.
+    </div>
+
+    <div class="about-section__socials">
+      <NuxtLink to="https://github.com/CyprienF" target="_blank">
+        <Github class="about-section__socials__icon" />
+      </NuxtLink>
+
+      <NuxtLink
+        to="https://www.linkedin.com/in/cyprien-floquet-095165109/"
+        target="_blank"
+      >
+        <Linkedin class="about-section__socials__icon" />
+      </NuxtLink>
+
+      <NuxtLink to="https://trakt.tv/users/cyprienf" target="_blank">
+        <Trakt class="about-section__socials__icon" />
+      </NuxtLink>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import picture from '@/assets/picture.jpg';
+
+import Github from '@/assets/icons/socials/github.svg';
+import Linkedin from '@/assets/icons/socials/linkedin.svg';
+import Trakt from '@/assets/icons/socials/trakt.svg';
+</script>
+
+<style lang="scss" scoped>
+.about-section {
+  display: flex;
+  flex-direction: column;
+
+  padding: 8rem 2rem 2rem 2rem;
+
+  width: 70%;
+
+  &__picture img {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    margin-bottom: 1rem;
+  }
+
+  &__title {
+    font-size: 2.5rem;
+    font-weight: 700;
+    margin-bottom: 1rem;
+  }
+
+  &__bio {
+    font-size: 1.1rem;
+    margin-bottom: 1rem;
+  }
+
+  &__socials {
+    display: flex;
+    gap: 1rem;
+
+    &__icon {
+      width: 2rem;
+      height: 2rem;
+    }
+  }
+}
+</style>
