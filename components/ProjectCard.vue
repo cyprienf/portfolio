807fdaa757926a34308370796142ab53f9d90700
CyprienF <floquet.cyprien@gmail.com>
2024-05-12 18:39:53 +0200

    feat: add skills on projects

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -3,6 +3,16 @@
     <p class="project-card__title">{{ project.title }}</p>
     <p class="project-card__description">{{ project.description }}</p>
 
+    <div class="project-card__skills">
+      <img
+        v-for="skill in project.skills"
+        :key="skill.id"
+        :src="`https://uyumfygdlasbgkivtobp.supabase.co/storage/v1/object/public/portfolio/icons/skills/${skill.logo}.png`"
+        :alt="skill.name"
+        class="project-card__skills__logo"
+      />
+    </div>
+
     <div class="project-card__links">
       <NuxtLink
         class="project-card__links__button"
@@ -50,6 +60,17 @@ defineProps<{
   &__description {
     margin-bottom: 0.75em;
     font-size: 0.95em;
+  }
+
+  &__skills {
+    display: flex;
+    gap: 0.3rem;
+
+    margin-bottom: 0.5rem;
+
+    &__logo {
+      width: 1.2rem;
+    }
   }
 
   &__links {
