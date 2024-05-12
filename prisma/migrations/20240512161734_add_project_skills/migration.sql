87cf7149e521e4b70227d442ab22c6cc59634a10
Cyprien FLOQUET <floquet.cyprien@gmail.com>
2024-05-12 18:40:34 +0200

    feat: add skills on projects (#29)

---
+++ prisma/migrations/20240512161734_add_project_skills/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "_ProjectToSkill" (
+    "A" SMALLINT NOT NULL,
+    "B" SMALLINT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ProjectToSkill_AB_unique" ON "_ProjectToSkill"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ProjectToSkill_B_index" ON "_ProjectToSkill"("B");
+
+-- AddForeignKey
+ALTER TABLE "_ProjectToSkill" ADD CONSTRAINT "_ProjectToSkill_A_fkey" FOREIGN KEY ("A") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ProjectToSkill" ADD CONSTRAINT "_ProjectToSkill_B_fkey" FOREIGN KEY ("B") REFERENCES "Skill"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- server/api/projects.ts
+++ server/api/projects.ts
@@ -3,7 +3,9 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async () => {
-  const data = await prisma.project.findMany();
+  const data = await prisma.project.findMany({
+    include: { skills: true },
+  });
 
   return data;
 });
